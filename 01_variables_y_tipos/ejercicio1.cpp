2842342b91a47d530b471c7aa4eab4c4aea2adf1
Matias <jcmatiasmunoz@gmail.com>
2026-03-26 00:09:28 -0500

    Agrega primer ejercicio de variables y tipos:) jejeje

---
+++ 01_variables_y_tipos/ejercicio1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, suma;
+
+    cout << "Ingresa el primer numero: ";
+    cin >> a;
+
+    cout << "Ingresa el segundo numero: ";
+    cin >> b;
+
+    suma = a + b;
+
+    cout << "La suma es: " << suma << endl;
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cpp-fundamentos
+Repositorio para practicar fundamentos de programación en C++.
+
+## Contenido
+- 01_variables_y_tipos
+
+## Primer ejercicio
+Programa que pide dos números y muestra su suma.
+
+## Segundo ejercicio
